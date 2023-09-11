cf402dd816f13753a2af025b977816b45e628aba
tuukka veteli <tuukkaveteli@gmail.com>
2023-09-11 22:43:03 +0300

    Deleted util file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use std::num::NonZeroU32;
+
 use simple_logger::SimpleLogger;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-
-#[path = "util/fill.rs"]
-mod fill;
 
 fn main() -> Result<(), impl std::error::Error> {
     SimpleLogger::new().init().unwrap();
@@ -17,6 +16,9 @@ fn main() -> Result<(), impl std::error::Error> {
         .with_title("tterm")
         .build(&event_loop)
         .unwrap();
+
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
 
     // elwt == event loop window target
     event_loop.run(move |event, _, control_flow| {
@@ -38,8 +40,22 @@ fn main() -> Result<(), impl std::error::Error> {
             Event::RedrawRequested(_) => {
                 //notify windowing system that we'll be presenting to the window
                 window.pre_present_notify();
-                fill::fill_window(&window);
 
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+                let mut buffer = surface.buffer_mut().unwrap();
+                buffer.fill(0x00181818);
+
+                buffer.present().unwrap();
                 println!("redrawing");
             }
             _ => (),
